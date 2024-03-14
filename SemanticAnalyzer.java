117dea33f22f3af9d28a684a3ff33ca599fefffc
ellamcivor <emcivorr@gmail.com>
2024-03-14 13:20:58 -0400

    delete method

--- SemanticAnalyzer.java
+++ SemanticAnalyzer.java
@@ -19,6 +19,92 @@ public class SemanticAnalyzer implements AbsynVisitor {
     table = new HashMap<String, ArrayList<NodeType>>();
   }
 
+  /* vistor methods */
+
+  public void visit(NameTy exp, int level ) {
+
+  }
+
+  public void visit( SimpleVar exp, int level ) {
+
+  }
+
+  public void visit( IndexVar exp, int level ) {
+
+  }
+
+  public void visit( NilExp exp, int level ) {
+
+  }
+
+  public void visit( IntExp exp, int level ) {
+
+  }
+
+  public void visit( BoolExp exp, int level ) {
+
+  }
+
+  public void visit( VarExp exp, int level ) {
+
+  }
+
+  public void visit( CallExp exp, int level ) {
+
+  }
+
+  public void visit( OpExp exp, int level ) {
+
+  }
+
+  public void visit( AssignExp exp, int level ) {
+
+  }
+
+  public void visit( IfExp exp, int level ) {
+
+  }
+
+  public void visit( WhileExp exp, int level ) {
+
+  }
+
+  public void visit( ReturnExp exp, int level ) {
+
+  }
+
+  public void visit( CompoundExp exp, int level ) {
+
+  }
+
+  public void visit( FunctionDec exp, int level ) {
+
+  }
+
+  public void visit( SimpleDec exp, int level ) {
+
+  }
+
+  public void visit( ArrayDec exp, int level ) {
+
+  }
+
+  public void visit( DecList exp, int level ) {
+
+  }
+
+  public void visit( VarDecList exp, int level ) {
+
+  }
+
+  public void visit( ExpList exp, int level ) {
+    
+  }
+
+
+
+  /* helper methods */
+
   private ArrayList lookup(String key) {
     if (table.containsKey(key)) {
       return table.get(key);
@@ -39,5 +125,18 @@ public class SemanticAnalyzer implements AbsynVisitor {
       table.put(key, nodeList);
     }
   }
+
+  private void delete(String key, NodeType node) {
+    if (table.containsKey(key)) {
+      ArrayList<NodeType> nodeList = table.get(key);
+      nodeList.remove(node);
+
+      if (nodeList.isEmpty()) {
+        table.remove(key);
+      }
+    }
+  }
+
+
 
 }
