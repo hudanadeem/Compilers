c37e4904103c1e5654feb760ce0e1376fcbb8de0
ellamcivor <emcivorr@gmail.com>
2024-03-16 23:43:11 -0400

    set up type checking but doesn't work

--- SemanticAnalyzer.java
+++ SemanticAnalyzer.java
@@ -17,6 +17,7 @@ public class SemanticAnalyzer implements AbsynVisitor {
 	private HashMap<String, ArrayList<NodeType>> table;
   final static int SPACES = 4;
   private int lastVisited = -1;
+  private String currentFunc = "";
 
 
   public SemanticAnalyzer() {
@@ -29,12 +30,21 @@ public class SemanticAnalyzer implements AbsynVisitor {
   }
 
   public void visit( SimpleVar simpleVar, int level ) {
-    System.out.println("visiting simpleVar");
+    lastVisited = lookup(simpleVar.name);
+    System.out.println("Simple var type: " + lastVisited + ", " + lookup(simpleVar.name));
+    // check if it has been declared
   }
 
   public void visit( IndexVar indexVar, int level ) {
     // if indexVar.index != int, report error where index is an expression
-    System.out.println("visiting indexVar");
+    indexVar.index.accept( this, level );
+    if (!isInt(lastVisited)) {
+      report_error("array index must be an integer");
+    }
+
+    // check if it has been declared
+
+    lastVisited = lookup(indexVar.name);
   }
 
   public void visit( NilExp exp, int level ) {
@@ -42,40 +52,190 @@ public class SemanticAnalyzer implements AbsynVisitor {
 
   public void visit( IntExp exp, int level ) {
     // exp type = int
+    lastVisited = NameTy.INT;
   }
 
   public void visit( BoolExp exp, int level ) {
     // exp type = bool
+    lastVisited = NameTy.BOOL;
   }
 
   public void visit( VarExp exp, int level ) {
     exp.variable.accept( this, level );
     // exp type = lookup(exp.name)
+    
   }
 
   public void visit( CallExp exp, int level ) {
 		exp.args.accept( this, level );
 
     // exp type = lookup(exp.func)
+    lastVisited = lookup(exp.func);
   }
 
   public void visit( OpExp exp, int level ) {
-		if (exp.left != null)
+    int ltype = -1, rtype = -1;
+
+		if (exp.left != null) {
 			 exp.left.accept( this, level );
-		if (exp.right != null)
+    }
+    ltype = lastVisited;
+
+		if (exp.right != null) {
 			 exp.right.accept( this, level );
+    }
+    rtype = lastVisited;
+    
+    switch( exp.op ) {
+
+			case OpExp.PLUS:
+			  if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("operands for addition must be integers");
+        }
+        else {
+          lastVisited = NameTy.INT;
+        }
+			  break;
+
+			case OpExp.MINUS:
+        if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("operands for subtraction must be integers");
+        }
+        else {
+          lastVisited = NameTy.INT;
+        }
+			  break;
+
+			case OpExp.TIMES:
+        if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("operands for multiplication must be integers");
+        }
+        else {
+          lastVisited = NameTy.INT;
+        }
+			  break;
+
+			case OpExp.DIV:
+        if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("operands for division must be integers");
+        }
+        else {
+          lastVisited = NameTy.INT;
+        }
+			  break;
+
+			case OpExp.EQ:
+        System.out.println("left " + ltype + " right " + rtype );
+        if ((ltype != rtype) || isVoid(ltype) || isVoid(rtype)) {
+          report_error("incompatable operands for boolean expression '=='");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.NE:
+			  if ((ltype != rtype) || isVoid(ltype) || isVoid(rtype)) {
+          report_error("incompatable operands for boolean expression '!='");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.LT:
+			  if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("incompatable operands for boolean expression '<'");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.LTE:
+        if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("incompatable operands for boolean expression '<='");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.GT:
+        if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("incompatable operands for boolean expression '>'");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.GTE:
+        if (!isInt(ltype) || !isInt(rtype)) {
+          report_error("incompatable operands for boolean expression '>='");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.NOT:
+        if (!isBool(rtype)) {
+          report_error("incompatable operands for boolean expression '~'");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.AND:
+        if (!isBool(ltype) || !isBool(rtype)) {
+          report_error("incompatable operands for boolean expression '&&'");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.OR:
+        if (!isBool(ltype) || !isBool(rtype)) {
+          report_error("incompatable operands for boolean expression '||'");
+        }
+        else {
+          lastVisited = NameTy.BOOL;
+        }
+			  break;
+
+			case OpExp.UMINUS:
+        if (!isInt(rtype)) {
+          report_error("operand must be of type integer");
+        }
+        else {
+          lastVisited = NameTy.INT;
+        }
+			  break;
+        
+		  }
   }
 
   public void visit( AssignExp exp, int level ) {
 		exp.lhs.accept( this, level );
+    int ltype = lastVisited;
 		exp.rhs.accept( this, level );
+    int rtype = lastVisited;
 
     // if lhs type != rhs type, report error
+    if (ltype != rtype) {
+      report_error("mismatch types for assign expression");
+    }
   }
 
   public void visit( IfExp exp, int level ) {
 		exp.test.accept( this, level );
     // if test != bool, report error
+    if (!isBool(lastVisited)) {
+      report_error("condition for if statement must be of boolean type");
+    } 
 
     indent(level);
     System.out.println("Entering a new block:");
@@ -101,6 +261,9 @@ public class SemanticAnalyzer implements AbsynVisitor {
   public void visit( WhileExp exp, int level ) {
     exp.test.accept( this, level+1 );
     // if type != bool or int, report error
+    if (!isInt(lastVisited) && !isBool(lastVisited)) {
+      report_error("condition for while expression must be of boolean type");
+    }
 
     indent(level);
     System.out.println("Entering a new block:");
@@ -118,6 +281,9 @@ public class SemanticAnalyzer implements AbsynVisitor {
 		}
 
     // if return type != lookup(function type), report error
+    if (lastVisited != lookup(currentFunc)) {
+      report_error("incompatable return type for function " + currentFunc);
+    }
   }
 
   public void visit( CompoundExp exp, int level ) {
@@ -131,6 +297,8 @@ public class SemanticAnalyzer implements AbsynVisitor {
     indent(level);
     System.out.println("Entering the scope for function " + exp.func + ":");
 
+    currentFunc = exp.func;
+
     insert(exp.func, new NodeType(exp.func, exp, level));
     
     exp.body.accept( this, level+1 );
@@ -139,20 +307,28 @@ public class SemanticAnalyzer implements AbsynVisitor {
     leaveScope(level+1);
     indent(level);
     System.out.println("Leaving the scope for function " + exp.func);
+    currentFunc = "";
   }
 
   public void visit( SimpleDec exp, int level ) {
-    exp.typ.accept( this, level );
+    // exp.typ.accept( this, level );
 
+    if (isVoid(exp.typ.typ)) {
+      report_error("cannot declare variable of type 'void'");
+    }
     insert(exp.name, new NodeType(exp.name, exp, level));
+      
     // if exp type = void, report error
   }
 
   public void visit( ArrayDec exp, int level ) {
 
+    if (isVoid(exp.typ.typ)) {
+      report_error("cannot declare variable of type 'void'");
+    }
     insert(exp.name, new NodeType(exp.name, exp, level));
   
-		exp.typ.accept( this, level );
+		// exp.typ.accept( this, level );
     // if exp type = void, report error
   }
 
@@ -187,6 +363,26 @@ public class SemanticAnalyzer implements AbsynVisitor {
 
   /*******  Helper Methods *******/
 
+  /* Reports error */
+  private void report_error(String message) {
+    System.out.println("Type error: " + message);
+  }
+
+  /* Returns true if type is integer */
+  private boolean isInt(int type) {
+    return (type == NameTy.INT);
+  }
+
+  /* Returns true if type is boolean */
+  private boolean isBool(int type) {
+    return (type == NameTy.BOOL);
+  }
+
+  /* Returns true if type is void */
+  private boolean isVoid(int type) {
+    return (type == NameTy.VOID);
+  }
+
   /* Prints each variable in specified scope then deletes from table */
   public void leaveScope(int scope) {
 
@@ -214,11 +410,13 @@ public class SemanticAnalyzer implements AbsynVisitor {
   }
 
   /* Looks up ArrayList with specific key */
-  private ArrayList lookup(String key) {
+  private int lookup(String key) {
     if (table.containsKey(key)) {
-      return table.get(key);
+      ArrayList<NodeType> id = table.get(key);
+      NodeType recent = id.get(id.size() - 1);
+      return recent.def.typ.typ;
     } else {
-      return null;
+      return -1;
     }
   }
 
